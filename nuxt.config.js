c7ca09c2fa669d58f4a8a304e0b9c70e5776c88c
Chester Alan <chesteralantagudin@gmail.com>
2020-09-30 17:40:27 +0800

    Updates

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'under-construction-page',
+    title: 'Website Is Under Construction',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
